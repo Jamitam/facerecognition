ad5852ad585cf584c8ed305985f0cb5476478a5e
Jamitam <tjozsa1@gmail.com>
2019-12-05 14:18:21 +0200

    some changes

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/register', {
+		fetch('https://still-fortress-04144.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
